d6cd3522704e2459513e80a3dc124719668a2913
FirstFileApp <93543116+2592003@users.noreply.github.com>
2022-12-20 23:26:52 +0530

    Create LinkedList_Creation.c

---
+++ LinkedList_Creation.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node*next;
+};
+void LinkedListTraversal(struct node*ptr)
+{
+    while (ptr!=NULL)
+    {
+        printf("%d\n",ptr->data);
+        ptr=ptr->next;
+    }
+    
+}
+int main()
+{
+    struct node*head = (struct node*)malloc(sizeof(struct node));
+    struct node*second = (struct node*)malloc(sizeof(struct node));
+    struct node*third = (struct node*)malloc(sizeof(struct node));
+    struct node*last = (struct node*)malloc(sizeof(struct node));
+
+    head->data=1;
+    head->next=second;
+
+    second->data=3;
+    second->next=third;
+
+    third->data=5;
+    third->next=last;
+
+    last->data=7;
+    last->next=NULL;
+
+    printf("Elements in Linked List:\n");
+    LinkedListTraversal(head);
+    return 0;
+}
